940ad22dbf40a7569b471cadd3154d0efdf44b0d
root <root@localhost.localdomain>
2023-03-30 10:52:27 +0800

    dev

--- perf.sh
+++ perf.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 make EXTRA_CFLAGS="-static"
-perf stat -e task-clock -e cycles -e context-switches -e migrations -e instructions -e page-faults,cache-misses,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,branch-load-misses,branch-loads,dTLB-load-misses,dTLB-loads,dTLB-store-misses,dTLB-stores,iTLB-load-misses,iTLB-loads ./http
+perf stat -e task-clock,context-switches,cpu-migrations,page-faults,cycles,stalled-cycles-frontend,stalled-cycles-backend,instructions,branches,branch-misses,L1-dcache-loads,L1-dcache-load-misses,LLC-loads,LLC-load-misses,dTLB-loads,dTLB-load-misses ./http

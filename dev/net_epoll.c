41e34c160312ac729afb0669fce663c1bf3db8ea
famin1234 <873354586@qq.com>
2025-05-02 23:20:50 +0800

    aa

--- dev/net_epoll.c
+++ dev/net_epoll.c
@@ -4,33 +4,33 @@
 #include "net.h"
 
 #define EPOLL_FD_MAX 1024
-struct net_loop_poll_t {
+struct net_epoll_t {
     int fd;
     int64_t            event_add;
     int64_t            event_mod;
     int64_t            event_del;
 };
 
-int net_loop_poll_init(struct net_loop_t *net_loop)
+int net_loop_epoll_init(struct net_loop_t *net_loop)
 {
-    struct net_loop_poll_t *net_loop_poll;
+    struct net_epoll_t *net_epoll;
 
-    net_loop_poll = (struct net_loop_poll_t *)mem_malloc(sizeof(struct net_loop_poll_t));
-    memset(net_loop_poll, 0, sizeof(struct net_loop_poll_t));
-    net_loop_poll->fd = epoll_create(EPOLL_FD_MAX);
-    if (net_loop_poll->fd < 0) {
-        LOG(LOG_ERROR, "epoll create fd=%d error: %s\n", net_loop_poll->fd, strerror(errno));
-        mem_free(net_loop_poll);
+    net_epoll = (struct net_epoll_t *)mem_malloc(sizeof(struct net_epoll_t));
+    memset(net_epoll, 0, sizeof(struct net_epoll_t));
+    net_epoll->fd = epoll_create(EPOLL_FD_MAX);
+    if (net_epoll->fd < 0) {
+        LOG(LOG_ERROR, "epoll create fd=%d error: %s\n", net_epoll->fd, strerror(errno));
+        mem_free(net_epoll);
         return -1;
     }
-    LOG(LOG_INFO, "epoll create fd=%d\n", net_loop_poll);
-    net_loop->arg = (void *)net_loop_poll;
+    LOG(LOG_INFO, "epoll create fd=%d\n", net_epoll);
+    net_loop->arg = (void *)net_epoll;
     return 0;
 }
 
-int net_loop_poll_add(struct net_loop_t *net_loop, struct conn_t *conn, int events)
+int net_loop_epoll_add(struct net_loop_t *net_loop, struct conn_t *conn, int events)
 {
-    struct net_loop_poll_t *net_loop_poll;
+    struct net_epoll_t *net_epoll;
     struct epoll_event ev;
 
     if (conn->events == events) {
@@ -44,46 +44,46 @@ int net_loop_poll_add(struct net_loop_t *net_loop, struct conn_t *conn, int even
     if (events & CONN_EVENT_WRITE) {
         ev.events |= EPOLLOUT;
     }
-    net_loop_poll = (struct net_loop_poll_t *)net_loop->arg;
+    net_epoll = (struct net_epoll_t *)net_loop->arg;
     if (conn->events == 0) {
-        if (epoll_ctl(net_loop_poll->fd, EPOLL_CTL_ADD, conn->sock, &ev)) {
-            LOG(LOG_ERROR, "fd=%d conn=%d add error: %s\n", net_loop_poll->fd, conn->sock, strerror(errno));
+        if (epoll_ctl(net_epoll->fd, EPOLL_CTL_ADD, conn->sock, &ev)) {
+            LOG(LOG_ERROR, "fd=%d conn=%d add error: %s\n", net_epoll->fd, conn->sock, strerror(errno));
             assert(0);
             return -1;
         }
-        net_loop_poll->event_add++;
+        net_epoll->event_add++;
     } else if (events > 0) {
-        if (epoll_ctl(net_loop_poll->fd, EPOLL_CTL_MOD, conn->sock, &ev)) {
-            LOG(LOG_ERROR, "fd=%d conn=%d mod error: %s\n", net_loop_poll->fd, conn->sock, strerror(errno));
+        if (epoll_ctl(net_epoll->fd, EPOLL_CTL_MOD, conn->sock, &ev)) {
+            LOG(LOG_ERROR, "fd=%d conn=%d mod error: %s\n", net_epoll->fd, conn->sock, strerror(errno));
             assert(0);
             return -1;
         }
-        net_loop_poll->event_mod++;
+        net_epoll->event_mod++;
     } else {
-        if (epoll_ctl(net_loop_poll->fd, EPOLL_CTL_DEL, conn->sock, &ev)) {
-            LOG(LOG_ERROR, "fd=%d conn=%d del error: %s\n", net_loop_poll->fd, conn->sock, strerror(errno));
+        if (epoll_ctl(net_epoll->fd, EPOLL_CTL_DEL, conn->sock, &ev)) {
+            LOG(LOG_ERROR, "fd=%d conn=%d del error: %s\n", net_epoll->fd, conn->sock, strerror(errno));
             assert(0);
             return -1;
         }
-        net_loop_poll->event_del++;
+        net_epoll->event_del++;
     }
     conn->events = events;
     return 0;
 }
 
-int net_loop_poll_del(struct net_loop_t *net_loop, struct conn_t *conn, int events)
+int net_loop_epoll_del(struct net_loop_t *net_loop, struct conn_t *conn, int events)
 {
     return 0;
 }
 
-int net_loop_poll_wait(struct net_loop_t *net_loop, int timeout)
+int net_loop_epoll_wait(struct net_loop_t *net_loop, int timeout)
 {
-    struct net_loop_poll_t *net_loop_poll = (struct net_loop_poll_t *)net_loop->arg;
+    struct net_epoll_t *net_epoll = (struct net_epoll_t *)net_loop->arg;
     struct epoll_event evs[EPOLL_FD_MAX];
     struct conn_t *conn;
     int i, nfds;
 
-    nfds = epoll_wait(net_loop_poll->fd, evs, EPOLL_FD_MAX, timeout);
+    nfds = epoll_wait(net_epoll->fd, evs, EPOLL_FD_MAX, timeout);
     for (i = 0; i < nfds; i++) {
         conn = evs[i].data.ptr;
         if (evs[i].events & (EPOLLIN | EPOLLERR | EPOLLHUP)) {
@@ -104,15 +104,15 @@ int net_loop_poll_wait(struct net_loop_t *net_loop, int timeout)
     return 0;
 }
 
-int net_loop_poll_uninit(struct net_loop_t *net_loop)
+int net_loop_epoll_uninit(struct net_loop_t *net_loop)
 {
-    struct net_loop_poll_t *net_loop_poll = (struct net_loop_poll_t *)net_loop->arg;
+    struct net_epoll_t *net_epoll = (struct net_epoll_t *)net_loop->arg;
 
-    LOG(LOG_INFO, "epoll close fd=%d\n", net_loop_poll->fd);
+    LOG(LOG_INFO, "epoll close fd=%d\n", net_epoll->fd);
     LOG(LOG_INFO, "event_add=%"PRId64" event_mod=%"PRId64" event_del=%"PRId64"\n",
-            net_loop_poll->event_add, net_loop_poll->event_mod, net_loop_poll->event_del);
-    close(net_loop_poll->fd);
-    mem_free(net_loop_poll);
+            net_epoll->event_add, net_epoll->event_mod, net_epoll->event_del);
+    close(net_epoll->fd);
+    mem_free(net_epoll);
     net_loop->arg = NULL;
     return 0;
 }
